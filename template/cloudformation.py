31e8cdead589457afe53889612c95648e38f7009
Mallik Sambaraju <msambaraju@gmail.com>
2017-02-16 20:34:36 -0500

    updated scripts

--- template/cloudformation.py
+++ template/cloudformation.py
@@ -3,6 +3,7 @@
 import time
 import getopt
 import sys
+import json
 
 # accesskey, secret,keyname, stackname, rollbacktimeout, waittime
 options, remainder = getopt.getopt(sys.argv[1:], '', ['accesskey=','secret=','keyname=','stackname=','rollbacktimeout=','waittime='])
@@ -21,12 +22,16 @@ for opt, arg in options:
     elif opt == '--waittime':
         waittime = arg
         
+'''
 print accessKey
 print secretKey
 print keyPairName
 print stackName
 print rollbacktimeout
 print waittime
+'''
+cloudFormationOutput = {}
+
 
 
 from boto.s3.connection import S3Connection
@@ -38,9 +43,8 @@ from boto.s3.key import Key
 bucketKey = Key(bucket)
 bucketKey.key = 'templatefile'
 bucketKey.set_contents_from_filename('docker_server.template')
-temp_url = bucketKey.generate_url(3600, query_auth=True, force_http=True)
 s3url = "https://s3.amazonaws.com/ipmstempbucket_0101/templatefile"
-print temp_url
+
 
 from boto.cloudformation.connection import CloudFormationConnection
 cfConn = CloudFormationConnection(accessKey, secretKey)
@@ -48,11 +52,11 @@ parameters = {}
 parameters["KeyName"] = keyPairName
 #stackName = "TestStack"
 disableRollback = False 
-print temp_url
+
 cfConn.create_stack(stackName, template_url = s3url, parameters= parameters.items(), 
                     disable_rollback = disableRollback, timeout_in_minutes = rollbacktimeout)
 
-print "Creating the stack"
+#print "Creating the stack"
 
 count = 0
 finished = False
@@ -62,21 +66,26 @@ while (count < 9 and finished == False):
         stack = stacks[0]
         if stack.stack_status == 'CREATE_COMPLETE':
             finished=True
-            print stack.stack_status
+            #print stack.stack_status
             for output in stack.outputs:
-                print('%s=%s (%s)' % (output.key, output.value, output.description))
+                #print('%s=%s (%s)' % (output.key, output.value, output.description))
+                cloudFormationOutput[output.key] = output.value
         elif stack.stack_status.endswith('_FAILED') or \
-             stack.stack_status in ('ROLLBACK_COMPLETE', 'UPDATE_ROLLBACK_COMPLETE'):
-            print "Failed to create stack found invalid status" + stack.stack_status
+            stack.stack_status in ('ROLLBACK_COMPLETE', 'UPDATE_ROLLBACK_COMPLETE'):
+            #print "Failed to create stack found invalid status" + stack.stack_status
             finished = True
             raise "Failed to create stack", stackName
         else:
-            print "Found In progress status" + stack.stack_status
-            print stack.stack_status
+            #print "Found In progress status" + stack.stack_status
+            #print stack.stack_status
             count = count+1
-            print "sleeping for some time"
+            #print "sleeping for some time"
             time.sleep(int(waittime)) 
-            print "woke up from sleep"
+            #print "woke up from sleep"
     else :
         finished = True
         raise "Invalid stack name", stackName
+
+cloudFormationOutputJson = json.dumps(cloudFormationOutput)
+
+print cloudFormationOutputJson
